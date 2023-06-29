60f8a0718fab6fb72d88ef206353448dd249b6be
Alexei Volovikov <alexei.volovikov@gmail.com>
2023-06-29 09:07:12 +0300

    Switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_06_111024) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.text "body"
     t.integer "likes_count", default: 0
