d6315bede627b6fbd1314ea0a8b448a605d6ed4b
Alexei Volovikov <alexei.volovikov@gmail.com>
2023-07-13 11:23:02 +0300

    Add ability to delete tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -56,7 +56,7 @@ class TweetsController < ApplicationController
     @tweet.destroy
 
     respond_to do |format|
-      #format.turbo_stream { render turbo_stream: turbo_stream.remove(@tweet) }
+      format.turbo_stream
       format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
       format.json { head :no_content }
     end
